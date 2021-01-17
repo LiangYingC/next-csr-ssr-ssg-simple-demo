ad4c0899c52afa46dded7460af5b812845430353
Liang Chen <liangchen@91app.com>
2021-01-17 22:23:38 +0800

    feat : add content to index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,11 @@
 import Layout from "../components/Layout";
 
 const Home = () => {
-  return <Layout />;
+  return (
+    <Layout>
+      <div>藉由簡單實作，更了解 CSR / SSR / SSG 差異。</div>
+    </Layout>
+  );
 };
 
 export default Home;
