3f9fd48b6a5a6204cccde89e0e2f882e6967cf8c
Liang Chen <liangchen@91app.com>
2021-06-07 20:17:21 +0800

    feat : fetch new animeQuotation api on csr page

--- pages/csr.jsx
+++ pages/csr.jsx
@@ -2,19 +2,20 @@ import { useState, useEffect } from "react";
 import Layout from "../components/Layout";
 
 const CSR = () => {
-  const [poetryData, setPoetryData] = useState({
+  const [animeQuotation, setAnimeQuotation] = useState({
     sentence: "Loading...",
     from: "Loading...",
   });
 
   useEffect(() => {
-    fetch("http://poetry.apiopen.top/sentences")
+    fetch("/api/animeQuotationData/random")
       .then((res) => res.json())
       .then((json) => {
-        const poetryResult = json.result;
-        setPoetryData({
-          sentence: poetryResult.name,
-          from: poetryResult.from,
+        const animeQuotationResult = json.result;
+
+        setAnimeQuotation({
+          sentence: animeQuotationResult.sentence,
+          from: animeQuotationResult.from,
         });
       })
       .catch((err) => {
@@ -24,7 +25,7 @@ const CSR = () => {
 
   return (
     <Layout>
-      <h3>Client Side Render</h3>
+      <h3>Client Side Render(CSR)</h3>
       <h4>
         當每次 user 進到頁面時，畫面開始在 Client 端製作並 render，API
         資料也是在 Client 才獲取（可以看到 Loading
@@ -34,13 +35,13 @@ const CSR = () => {
       <li>特點二：每次可取得 API 最新資料。</li>
       <li>特點三：user 可以很快看到畫面，但畫面是漸變，非一次到位。</li>
       <ul>
-        <p>以下為同支 API 回傳的資料，可以觀察與 SSR/SSG 的差異：</p>
-        <li>詩句：{poetryData.sentence}</li>
-        <li>出處：{poetryData.from}</li>
-        <li>詩句：{poetryData.sentence}</li>
-        <li>出處：{poetryData.from}</li>
-        <li>詩句：{poetryData.sentence}</li>
-        <li>出處：{poetryData.from}</li>
+        <p>以下為同支 API 回傳的資料，可以觀察與 CSR/SSR/SSG/ISR 的差異：</p>
+        <li>名句：{animeQuotation.sentence}</li>
+        <li>出處：{animeQuotation.from}</li>
+        <li>名句：{animeQuotation.sentence}</li>
+        <li>出處：{animeQuotation.from}</li>
+        <li>名句：{animeQuotation.sentence}</li>
+        <li>出處：{animeQuotation.from}</li>
       </ul>
     </Layout>
   );
